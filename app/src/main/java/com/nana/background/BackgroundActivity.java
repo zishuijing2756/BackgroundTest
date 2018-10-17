f01fe86a4fd2a24ac60880208727003617585cdc
yangnn <yangnn1@guahao.com>
2018-10-17 17:56:45 +0800

    Initial commit

---
+++ app/src/main/java/com/nana/background/BackgroundActivity.java
@@ -0,0 +1,13 @@
+package com.nana.background;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BackgroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background);
+    }
+}
